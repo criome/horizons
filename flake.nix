6d16efc2019804d044087ec26c55f4fd01b345e4
li <li@maisiliym.criome.me>
2025-03-11 11:06:54 +0100

    add-dep(rust-atom)

--- flake.nix
+++ flake.nix
@@ -4,11 +4,13 @@
   inputs = {
     atom.url = "github:LiGoldragon/atom/atomicFlake-v1";
     system.url = "github:criome/system";
-    rust-atom.url = "github:criome/rust-atom";
 
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-atom.url = "github:criome/nixpkgs-atom";
     nixpkgs-atom.inputs.nixpkgs.follows = "nixpkgs";
+
+    rust-atom.url = "github:criome/rust-atom";
+    rust-atom.inputs.nixpkgs.follows = "nixpkgs";
 
     horizons-rs.url = "github:criome/horizons-rs";
     horizons-rs.flake = false;
